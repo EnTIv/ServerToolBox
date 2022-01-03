9158fb42725e7ea08c6cda46e4edc64831cff803
EnTIv <1522935501@qq.com>
2022-01-03 22:31:01 +0800

    新增清理屏幕功能

---
+++ src/main/java/com/entiv/servertoolbox/module/CleanChat.java
@@ -0,0 +1,43 @@
+package com.entiv.servertoolbox.module;
+
+import com.entiv.servertoolbox.Module;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class CleanChat extends Module implements Listener, CommandExecutor {
+
+    private static final String CLEAN_MESSAGE = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+
+
+    @Override
+    protected void onEnable() {
+        registerListener();
+
+        registerCommand("CleanChat");
+    }
+
+    @EventHandler
+    private void onJoin(PlayerJoinEvent event) {
+        final Player player = event.getPlayer();
+
+        for (int i = 0; i < 10; i++) {
+            player.sendMessage(CLEAN_MESSAGE);
+        }
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            sender.sendMessage(CLEAN_MESSAGE);
+        }
+
+        return false;
+    }
+}
